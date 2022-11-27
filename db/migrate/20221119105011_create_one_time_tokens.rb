cffc8eb8f9b4732182b49009242239b503d9a73c
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-11-27 18:06:25 +0900

    exchange_tokenの型をtextからjsonに変更

--- db/migrate/20221119105011_create_one_time_tokens.rb
+++ db/migrate/20221119105011_create_one_time_tokens.rb
@@ -2,7 +2,7 @@ class CreateOneTimeTokens < ActiveRecord::Migration[7.0]
   def change
     create_table :one_time_tokens, id: false do |t|
       t.string :id, limit: 36, null: false, primary_key: true
-      t.string :exchange_token
+      t.string :exchange_json
       t.datetime :expires_at
     end
   end
