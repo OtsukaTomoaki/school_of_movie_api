73874a5593c0a7be2a1bff4d33b0763456522328
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-12-02 01:46:05 +0900

    ユーザidは初めからuuidにする

--- db/migrate/20221030141506_create_users.rb
+++ db/migrate/20221030141506_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :users, id: false do |t|
+      t.string :id, limit: 36, null: false, primary_key: true
       t.string :name
       t.string :email
       t.string :password_digest
