d75b8de1e5994cf6df106a3e07e5018554ecd2b3
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-12-18 18:39:36 +0900

    エラーメッセージを返すように修正

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::SessionsController < Api::V1::ApplicationController
       user.remember
       render json: { token: token, remember_token: user.remember_token }, status: :ok
     else
-      render json: user.errors, status: :unauthorized
+      render json: { message: 'emailまたはpasswordが正しくありません' }, status: :unauthorized
     end
   end
 
