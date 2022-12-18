6a001d040f08fc9fbe1d599e12ec8f22e302d4d6
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-12-18 18:39:55 +0900

    jwtを取得するエンドポイントのテストコードを追加

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,7 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) {
+    FactoryBot.create(:user,
+    name: 'signin shitatou',
+    email: "shitatou@gmail.com",
+    password: 'shitatou1234')
+  }
+
+  let!(:signin_params) {
+    {
+      email: 'shitatou@gmail.com',
+      password: 'shitatou1234'
+    }.to_json
+  }
+
+  let!(:invalid_email_signin_params) {
+    {
+      email: 'shitatou_ze@gmail.com',
+      password: 'shitatou1234'
+    }.to_json
+  }
+
+  let!(:invalid_password_signin_params) {
+    {
+      email: 'shitatou@gmail.com',
+      password: 'shitatou12345678'
+    }.to_json
+  }
+
+  let!(:signin_headers) {
+    {
+      'Content-Type'=> 'application/json'
+    }
+  }
+
+  context 'サインイン処理の検証' do
+    it '正しいID, Passwordをリクエストした場合、トークンを取得できる' do
+      post '/api/v1/sessions', params: signin_params, headers: signin_headers
+
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['token'].split('.').length).to be >= 3
+      expect(json['remember_token']).to_not be_empty
+    end
+
+    it '誤ったID, 正しいPasswordをリクエストした場合、トークンを取得できる' do
+      post '/api/v1/sessions', params: invalid_email_signin_params, headers: signin_headers
+
+      expect(response).to have_http_status 401
+      json = JSON.parse(response.body)
+      expect(json['message']).to eq('emailまたはpasswordが正しくありません')
+    end
+
+    it '正しいID, 誤ったPasswordをリクエストした場合、トークンを取得できる' do
+      post '/api/v1/sessions', params: invalid_password_signin_params, headers: signin_headers
+
+      expect(response).to have_http_status 401
+      json = JSON.parse(response.body)
+      expect(json['message']).to eq('emailまたはpasswordが正しくありません')
+    end
   end
 end
