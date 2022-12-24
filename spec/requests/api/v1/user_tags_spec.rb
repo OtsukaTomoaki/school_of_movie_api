0b2b705aec4ca92e511c2f9d511f8a6b0b66be70
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-12-24 10:29:01 +0900

    既存のspecのリファクタ

--- spec/requests/api/v1/user_tags_spec.rb
+++ spec/requests/api/v1/user_tags_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::UserTags", type: :request, authentication: :skip  do
-  context 'タグ追加処理の検証' do
+  describe 'post' do
     let!(:tags) {
       tag = {
         tag: "うわぁぁぁぁぁ!!!!!!!"
       }
     }
-    it 'タグを追加できる' do
-      post '/api/v1/user_tags', params: tags
+    context 'タグの新規追加' do
+      it 'タグが登録されていること' do
+        post '/api/v1/user_tags', params: tags
 
-      expect(response).to have_http_status 200
-      json = JSON.parse(response.body)
-      expect(json['tag']).to eq 'うわぁぁぁぁぁ!!!!!!!'
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json['tag']).to eq 'うわぁぁぁぁぁ!!!!!!!'
+      end
     end
   end
 end
