c45a8efb5c0f4352fb5dff571fc828cea1ade6da
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-12-29 17:54:39 +0900

    テストデータの変更

--- spec/requests/api/v1/user_tags_spec.rb
+++ spec/requests/api/v1/user_tags_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::UserTags", type: :request, authentication: :skip  do
+  describe 'index' do
+    it 'タグの一覧が取得できること' do
+      get '/api/v1'
+    end
+  end
   describe 'post' do
     let!(:tags) {
       tag = {
-        tag: "うわぁぁぁぁぁ!!!!!!!"
+        tag: "sample!!!!!!!"
       }
     }
     context 'タグの新規追加' do
@@ -13,7 +18,7 @@ RSpec.describe "Api::V1::UserTags", type: :request, authentication: :skip  do
 
         expect(response).to have_http_status 200
         json = JSON.parse(response.body)
-        expect(json['tag']).to eq 'うわぁぁぁぁぁ!!!!!!!'
+        expect(json['tag']).to eq 'sample!!!!!!!'
       end
     end
   end
