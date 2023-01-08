f35b9bc96eb56a637970feb93b5ae752ebcbe858
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-01-09 01:25:30 +0900

    FactoryBotを使用するように修正

--- spec/services/authentication_service_spec.rb
+++ spec/services/authentication_service_spec.rb
@@ -4,16 +4,13 @@ RSpec.describe AuthenticationService, type: :service do
   pending "add some examples to (or delete) #{__FILE__}"
 
   let!(:user) do
-    User.create({
+    FactoryBot.create(:user,
       name: 'Nancy Molgan',
       email: 'nancy-pancy-denpcy@gmail.com',
-      password: 'saikyounoP@ssw0rd',
-      password_confirmation: 'saikyounoP@ssw0rd',
-      activated: true,
-      activated_at: Time.zone.now})
+      password: 'saikyounoP@ssw0rd')
   end
 
-  context "ログイン" do
+  context "authenticate_user_with_password!" do
     it "email, passwordが正しい場合" do
       expect(AuthenticationService.authenticate_user_with_password!(
         'nancy-pancy-denpcy@gmail.com',
