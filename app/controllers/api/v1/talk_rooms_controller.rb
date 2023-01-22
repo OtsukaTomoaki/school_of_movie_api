4677d39de3099bbd7fe5c78818bb82c727b8dfaa
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-01-22 18:30:04 +0900

    削除機能の追加

--- app/controllers/api/v1/talk_rooms_controller.rb
+++ app/controllers/api/v1/talk_rooms_controller.rb
@@ -16,6 +16,23 @@ class Api::V1::TalkRoomsController < Api::V1::ApplicationController
     end
   end
 
+  def create
+    @form = TalkRooms::Form.new(talk_room_params)
+    if @form.valid?
+      @talk_room = TalkRoomService.new.create!(form: @form, owner_user: current_user)
+    else
+      response_bad_request(errors: @form.error_messages)
+    end
+  end
+
+  def destroy
+    @id = params[:id]
+    ActiveRecord::Base.transaction do
+      TalkRoomPermission.where(talk_room_id: @id).destroy_all
+      TalkRoom.find_by_id(@id).destroy!
+    end
+  end
+
   private
     def talk_room_params
       params.require(:talk_room).permit(:name, :describe, :status)
