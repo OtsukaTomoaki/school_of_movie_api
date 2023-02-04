cbdc1705865876be33a09796985de0f3325dde3e
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-02-04 19:20:55 +0900

    メッセージの取得時にはユーザ名を返すようにする

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MessagesController < Api::V1::ApplicationController
   def index
     talk_room = TalkRoom.get(id: params[:talk_room_id], user: current_user)
     if talk_room.present?
-      @messages = Message.where(talk_room_id: talk_room.id).order(:created_at)
+      @messages = Message.includes(:user).where(talk_room_id: talk_room.id).order(:created_at)
     else
       response_not_found
     end
