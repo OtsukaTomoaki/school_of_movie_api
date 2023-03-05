384b481630ca19964d18de407a6f461ce13c07ec
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-03-05 16:47:45 +0900

    redisのバージョンアップに伴うURI の指定方法変更対応

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,10 @@
-Redis.current = Redis.new(url: "redis://#{ENV['REDIS_URL']}")
+# Redis.current = Redis.new(url: "redis://#{ENV['REDIS_URL']}")
+# Redis クライアントの初期化
+redis_client = Redis.new(url: "redis://#{ENV['REDIS_URL']}")
+
+# グローバル変数に設定
+Rails.application.config.redis = redis_client
+
+# # グローバルでの使用
+# Rails.application.config.redis.get("key_name")
+# Rails.application.config.redis.set("key_name", "value")
