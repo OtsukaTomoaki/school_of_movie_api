f892ed7e0beb1fb1abcbc9f2aa1827569098f080
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-03-06 23:25:32 +0900

    メッセージの内容を形態素解析する

--- app/batch/analysis/messages.rb
+++ app/batch/analysis/messages.rb
@@ -2,6 +2,10 @@ class Messages
   require 'logger'
   require 'natto'
 
+  EXTRACT_WORD_TYPES = ['名詞', '固有名詞'].freeze
+  EXTRACT_WORD_SORTS = ['一般', '組織'].freeze
+  EXCLUDES_WORDS_SORTS = ['非自立', '接尾', '代名詞'].freeze
+
   def self.say
     puts "Test"
     logger = Logger.new(Rails.root.join('log', 'cron.log'))
@@ -9,7 +13,6 @@ class Messages
   end
 
   def initialize
-    puts "initialize"
   end
 
   def execute
@@ -17,14 +20,23 @@ class Messages
   end
 
   private
-    def _execute
-      p '_execute'
-      p Message.count
-      txt = '公務員だぞ、地方公務員。お前達が乗車しているのはグレートマジンガーか？ ダンガイオーか？ 自閉症児や不良少年が主人公のロボットアニメじゃないんだよ。分かっとるのか？ 本当に'
-      p txt
-      natto = Natto::MeCab.new('/usr/lib/aarch64-linux-gnu/libmecab.so.2')
-      natto.parse(txt) do |n|
-        puts "#{n.surface}: #{n.feature}"
+
+  def _execute
+    # p Message.count
+    natto = Natto::MeCab.new
+    messages = Message.all
+    messages.each { |message|
+      natto.parse(message.content) do |n|
+        if include_word?(n)
+          puts n.surface
+        end
       end
-    end
+    }
+  end
+  def include_word?(natto_text)
+    natto_words = natto_text.feature.split(',')
+    EXTRACT_WORD_TYPES.include?(natto_words.first) &&
+    EXTRACT_WORD_SORTS.any? { |word| natto_words.include?(word) } &&
+    !EXCLUDES_WORDS_SORTS.any? { |word| natto_words.include?(word) }
+  end
 end
