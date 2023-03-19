f9113517d3457b060c18f89c4f4a99a6dbb930b8
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-03-19 17:06:12 +0900

    taskでgenre/movieのエンドポイントを呼び出すようにする

--- spec/lib/api/the_movie_database/tasks/movies_spec.rb
+++ spec/lib/api/the_movie_database/tasks/movies_spec.rb
@@ -3,17 +3,17 @@ require 'rake'
 Rails.application.load_tasks
 
 require_relative "#{Rails.root}/lib/api/the_movie_database/client"
-require_relative "#{Rails.root}/lib/api/the_movie_database/response_importer"
+require_relative "#{Rails.root}/lib/api/the_movie_database/movie_importer"
 
 RSpec.describe 'movies:fetch_popular_movie' do
   let(:movies_client) { instance_double(Api::TheMovieDatabase::Client) }
-  let(:response_importer) { instance_double(Api::TheMovieDatabase::ResponseImporter) }
+  let(:movie_importer) { instance_double(Api::TheMovieDatabase::MovieImporter) }
   let(:request_max_times) { 5 }
 
   before do
     allow(Api::TheMovieDatabase::Client).to receive(:new).and_return(movies_client)
-    allow(Api::TheMovieDatabase::ResponseImporter).to receive(:new).and_return(response_importer)
-    allow(response_importer).to receive(:execute!)
+    allow(Api::TheMovieDatabase::MovieImporter).to receive(:new).and_return(movie_importer)
+    allow(movie_importer).to receive(:execute!)
 
     ENV['THE_MOVIE_DATABASE_REQUEST_MAX_TIMES'] = request_max_times.to_s
   end
@@ -22,18 +22,23 @@ RSpec.describe 'movies:fetch_popular_movie' do
   }
   context "ページ数が1000件で取得する最大ページ数が3までの場合" do
     it 'fetch_popular_listが3回呼び出されること' do
+      allow(movies_client).to receive(:fetch_movie_genres).and_return({'genres' => []})
       allow(movies_client).to receive(:fetch_popular_list).and_return({'total_pages' => 1000})
 
+      expect(movies_client).to receive(:fetch_movie_genres).exactly(1).times
       expect(movies_client).to receive(:fetch_popular_list).exactly(request_max_times).times
       expect_any_instance_of(Kernel).to receive(:sleep).with(5).exactly(5).times
 
       Rake::Task['movies:fetch_popular_movie'].invoke
     end
 
-    it 'Api::TheMovieDatabase::ResponseImporterのインスタンスが3回生成されること' do
+    it 'Api::TheMovieDatabase::MovieImporterのインスタンスが3回生成されること' do
+      allow(movies_client).to receive(:fetch_movie_genres).and_return({'genres' => []})
       allow(movies_client).to receive(:fetch_popular_list).and_return({'total_pages' => 1000})
-      expect(Api::TheMovieDatabase::ResponseImporter).to receive(:new).exactly(request_max_times).times
-      expect(response_importer).to receive(:execute!).exactly(request_max_times).times
+
+      expect(movies_client).to receive(:fetch_movie_genres).once
+      expect(Api::TheMovieDatabase::MovieImporter).to receive(:new).exactly(request_max_times).times
+      expect(movie_importer).to receive(:execute!).exactly(request_max_times).times
       expect_any_instance_of(Kernel).to receive(:sleep).with(5).exactly(5).times
 
       Rake::Task['movies:fetch_popular_movie'].invoke
@@ -42,12 +47,14 @@ RSpec.describe 'movies:fetch_popular_movie' do
 
   context "ページ数が2件で取得する最大ページ数が3までの場合" do
     it 'fetch_popular_listが2回呼び出されること' do
+      allow(movies_client).to receive(:fetch_movie_genres).and_return({'genres' => []})
       allow(movies_client).to receive(:fetch_popular_list).and_return({'total_pages' => 2})
 
+      expect(movies_client).to receive(:fetch_movie_genres).once
       expect(movies_client).to receive(:fetch_popular_list).ordered.with(page: 1)
       expect(movies_client).to receive(:fetch_popular_list).ordered.with(page: 2)
 
-      expect(response_importer).to receive(:execute!).twice
+      expect(movie_importer).to receive(:execute!).twice
 
       expect_any_instance_of(Kernel).to receive(:sleep).with(5).once
 
