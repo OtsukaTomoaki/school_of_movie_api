3c27485382ba6e90b79547e9198fcbbd3318d322
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-04-15 23:58:46 +0900

    sleep時間の修正

--- spec/lib/api/the_movie_database/tasks/movies_spec.rb
+++ spec/lib/api/the_movie_database/tasks/movies_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'movies:fetch_popular_movie' do
   let(:movies_client) { instance_double(Api::TheMovieDatabase::Client) }
   let(:importer) { instance_double(Api::TheMovieDatabase::Importer) }
   let(:request_max_times) { 5 }
-
+  let(:sleep_time) { 5 }
   before do
     allow(Api::TheMovieDatabase::Client).to receive(:new).and_return(movies_client)
     allow(Api::TheMovieDatabase::Importer).to receive(:new).and_return(importer)
@@ -21,25 +21,25 @@ RSpec.describe 'movies:fetch_popular_movie' do
     Rake::Task['movies:fetch_popular_movie'].reenable
   }
   context "ページ数が1000件で取得する最大ページ数が3までの場合" do
-    it 'fetch_popular_listが3回呼び出されること' do
+    it 'fetch_popular_listが5回呼び出されること' do
       allow(movies_client).to receive(:fetch_movie_genres).and_return({'genres' => []})
       allow(movies_client).to receive(:fetch_popular_list).and_return({'total_pages' => 1000})
 
       expect(movies_client).to receive(:fetch_movie_genres).exactly(1).times
       expect(movies_client).to receive(:fetch_popular_list).exactly(request_max_times).times
-      expect_any_instance_of(Kernel).to receive(:sleep).with(5).exactly(5).times
+      expect_any_instance_of(Kernel).to receive(:sleep).with(sleep_time).exactly(5).times
 
       Rake::Task['movies:fetch_popular_movie'].invoke
     end
 
-    it 'Api::TheMovieDatabase::Importerのインスタンスが3回生成されること' do
+    it 'Api::TheMovieDatabase::Importerのインスタンスが5回生成されること' do
       allow(movies_client).to receive(:fetch_movie_genres).and_return({'genres' => []})
       allow(movies_client).to receive(:fetch_popular_list).and_return({'total_pages' => 1000})
 
       expect(movies_client).to receive(:fetch_movie_genres).once
       expect(Api::TheMovieDatabase::Importer).to receive(:new).exactly(request_max_times).times
       expect(importer).to receive(:execute!).exactly(request_max_times).times
-      expect_any_instance_of(Kernel).to receive(:sleep).with(5).exactly(5).times
+      expect_any_instance_of(Kernel).to receive(:sleep).with(sleep_time).exactly(5).times
 
       Rake::Task['movies:fetch_popular_movie'].invoke
     end
@@ -56,7 +56,7 @@ RSpec.describe 'movies:fetch_popular_movie' do
 
       expect(importer).to receive(:execute!).twice
 
-      expect_any_instance_of(Kernel).to receive(:sleep).with(5).once
+      expect_any_instance_of(Kernel).to receive(:sleep).with(sleep_time).once
 
       Rake::Task['movies:fetch_popular_movie'].invoke
     end
