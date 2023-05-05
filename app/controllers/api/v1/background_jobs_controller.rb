74c5d3ad262aff0ead0f00a56c90daef9c513719
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-05-05 17:06:21 +0900

    バックグラウンドの一覧APIの実装

---
+++ app/controllers/api/v1/background_jobs_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::BackgroundJobsController < Api::V1::ApplicationController
+  def index
+    # BackGroundJobの一覧を取得する
+    status = params[:status]
+    job_type = params[:job_type]
+    page = params[:page].present? ? params[:page] : 1
+    per_page = params[:per_page].present? ? params[:per_page] : 10
+
+    @background_jobs = BackgroundJob.search(status: status, job_type: job_type, page: page, per: per_page)
+  end
+end
