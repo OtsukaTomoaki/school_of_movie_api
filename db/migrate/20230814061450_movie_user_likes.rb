bd82cf94a358618b943a86b383e7f6843d60aaf6
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-14 15:30:09 +0900

    migration ファイルの作成

---
+++ db/migrate/20230814061450_movie_user_likes.rb
@@ -0,0 +1,14 @@
+class MovieUserLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movie_user_likes, id: false do |t|
+      t.string :id, limit: 36, null: false, primary_key: true
+      t.string :user_id, limit: 36, null: false, comment: 'ユーザーID'
+      t.string :movie_id, limit: 36, null: false, comment: '映画ID'
+      t.timestamps
+    end
+
+    add_index :movie_likes, [:user_id, :movie_id], unique: true
+    add_index :movie_likes, [:user_id], unique: false
+    add_index :movie_likes, [:movie_id], unique: false
+  end
+end
