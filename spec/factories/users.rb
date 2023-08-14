9f2ac34d1ee71bd4094c6a62612dec2229bc8c32
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-14 18:54:12 +0900

    不要な列のfactoryを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8)}
-    password_digest { 'hoge' }
-    activation_digest { 'hoge' }
     activated { true }
   end
 end
