9e83768bbde89504641da7598b1594f4637f8055
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-14 16:36:27 +0900

    userのfactoryを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
+  require 'faker'
+
   factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 8)}
+    password_digest { 'hoge' }
+    activation_digest { 'hoge' }
+    activated { true }
   end
 end
