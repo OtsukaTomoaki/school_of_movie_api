d89cc0bd94aedf18106b5ba26ed7fd2c27268200
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-14 18:24:13 +0900

    不要なコードの削除

--- app/models/movie_user_like.rb
+++ app/models/movie_user_like.rb
@@ -4,17 +4,4 @@ class MovieUserLike < ApplicationRecord
   validates :movie_id, presence: true
   validates :user_id, presence: true
   validates :movie_id, uniqueness: { scope: :user_id }
-
-  def self.search(user_id: nil, movie_id: nil)
-    movie_user_likes = MovieUserLike
-    if user_id.present?
-      movie_user_likes = movie_user_likes.where(user_id: user_id)
-    end
-
-    if movie_id.present?
-      movie_user_likes = movie_user_likes.where(movie_id: movie_id)
-    end
-
-    movie_user_likes.order(created_at: :desc)
-  end
 end
