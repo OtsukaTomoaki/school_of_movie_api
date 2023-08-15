888b68a855ddc983469541bcf388621924f323bb
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-15 13:56:03 +0900

    映画のlikeするAPIのボディのテストコード追加

--- spec/requests/api/v1/movie_user_likes/create_spec.rb
+++ spec/requests/api/v1/movie_user_likes/create_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::MovieUserLikes", type: :request, authentication: :skip do
   describe 'POST #update' do
-    let!(:user) { FactoryBot.create(:user) }
+    let!(:user) { authenticated_user }
     let!(:movie) { FactoryBot.create(:movie) }
     let!(:params) do
       {
@@ -19,6 +19,13 @@ RSpec.describe "Api::V1::MovieUserLikes", type: :request, authentication: :skip 
         it 'レスポンスが200になり、MovieUserLikeが作成されること' do
           expect { subject }.to change(MovieUserLike, :count).by(1)
           expect(response).to have_http_status 200
+          json_body = JSON.parse(response.body)
+          expect(json_body['movie_id']).to eq movie.id
+          expect(json_body['user_id']).to eq user.id
+
+          last_movie_user_like = MovieUserLike.all.order(created_at: :desc).first
+          expect(json_body['id']).to eq last_movie_user_like.id
+          expect(json_body['created_at']).to eq last_movie_user_like.created_at.strftime('%Y-%m-%dT%H:%M:%S.%LZ')
         end
       end
     end
