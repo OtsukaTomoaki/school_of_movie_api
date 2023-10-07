2b12117a03724bcc1ee1996388925aa12bdc97d6
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-10-08 01:06:47 +0900

    継承元の誤りを修正

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,14 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = TokenService.authorization(request.cookies["authorization"])
+      self.current_user = TokenService.authorization(authorization_token)
+    end
+
+    def authorization_token
+      return @authorization_token if @authorization_token.present?
+      auth_header = request.headers['Authorization'].present? ? request.headers['Authorization'] : request.cookies["authorization"]
+      @authorization_token = auth_header.split(' ').last if auth_header
+      @authorization_token
     end
   end
 end
