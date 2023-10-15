512017a6ed3dfe5a50a1d224d0a2f41a43a77fa3
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-10-16 01:45:49 +0900

    認証をスキップするようにする

--- app/controllers/api/v1/health_check_controller.rb
+++ app/controllers/api/v1/health_check_controller.rb
@@ -1,4 +1,6 @@
-class Api::V1::HealthCheckController < ApplicationController
+class Api::V1::HealthCheckController < Api::V1::ApplicationController
+  skip_before_action :current_user, only: [:index]
+
   def index
     render json: { success: true }
   end
